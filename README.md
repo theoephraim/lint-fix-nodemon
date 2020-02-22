fc3ca9f8896c601e9ecb055e094b63069318da1c
Theo Ephraim <theozero@gmail.com>
2020-02-22 16:05:08 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # lint-fix-nodemon
+
+> Watch your files and run ESLint + fix + nodemon on save
+
+**WHY DO YOU NEED THIS?**
+
+1. Avoid a double restart when eslint fixes your files (nodemon would normally detect the initial change and then the fix change)
+2. Don't get stuck if linting or starting the server fails - we just keep watching and trying again on each save
+
+### Installation
+`npm install lint-fix-nodemon --save-dev` or `yarn add lint-fix-nodemon -D`
+
+### Setup
+You can run it with `npx lint-fix-nodemon` or normally you should just add a script to your package.json file.
+
+_I usually name it "dev" (`"dev": "lint-fix-nodemon"`) so you can run `npm run dev`_
+
+### Configuration
+
+By default, it will assume nodemon should run the script specified as your "main" in your package.json file.
+Otherwise you can also pass it in as an argument -- for example: `lint-fix-nodemon ./api/start.js`
+
+Other configuration in terms of what files to watch and lint will be read from the [nodemonConfig](https://github.com/remy/nodemon#packagejson) entry in your package.json file
+
+It will automatically ignore your node_modules folder and any files/folders that start with "."
+
+
+### An example package.json file
+```
+{
+  ...
+  "scripts": {
+    "dev": "lint-fix-nodemon api/start.js",
+    ...
+  },
+  "nodemonConfig": {
+    watch: ["api/"],
+    ignore: ["api/scripts"]
+  },
+  ...
+}
+```
---
+++ index.js
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+
+// This script just watches files and chains a run of eslint (with fixes enabled) and then restarting nodemon
+// However we fix 2 problems that are common with using these packages, especially together
+// 1 - when eslint fixes files, we dont trigger an extra nodemon restart (we skip the first one by hooking into eslint and detecting fixes)
+// 2 - if linting fails or starting the server fails, we still just watch the files and start/restart properly, rather than dying
+
+const fs = require('fs');
+const _ = require('lodash');
+const nodemon = require('nodemon');
+const chokidar = require('chokidar');
+const { CLIEngine } = require('eslint');
+const colors = require('colors');
+
+const [node, script, ...args] = process.argv; // eslint-disable-line no-unused-vars
+
+// try to load config from package.json - looking for `nodemonConfig` key
+const packageJson = JSON.parse(fs.readFileSync(__dirname + '/../../package.json'));
+const { nodemonConfig } = packageJson;
+
+// you can pass in the script to run or it will default to the "main" in package.json
+const mainScript = args[0] || packageJson.main;
+
+
+const eslintCLI = new CLIEngine({
+  fix: true,
+  cache: true,
+  ignorePath: '.gitignore',
+});
+const formatter = eslintCLI.getFormatter();
+
+
+
+let appProcess;
+function runLinter() {
+  // run eslint w/ fixes
+  const report = eslintCLI.executeOnFiles(nodemonConfig.watch);
+  CLIEngine.outputFixes(report);
+
+  // output Lint results
+  console.log(formatter(report.results));
+
+  // return info on whether any fixes were made
+  return {
+    fixes: _.some(report.results, 'output'),
+    fatal: _.some(report.results, (file) => _.some(file.messages, { fatal: true })),
+  };
+}
+function startServer() {
+  if (process.env.LINT_ONLY) return;
+  if (appProcess) {
+    console.log('>> Restarting server <<'.green);
+    appProcess.restart();
+  } else {
+    console.log('>> Starting server <<'.green);
+    appProcess = nodemon({
+      script: mainScript,
+      watch: false,
+      inspect: true,
+    });
+  }
+}
+function stopServer() {
+  nodemon.emit('quit');
+  appProcess = null;
+}
+
+
+let ignoreNextChange = false;
+
+function lintAndRun() {
+  if (ignoreNextChange) {
+    ignoreNextChange = false;
+    return;
+  }
+
+  const lintResults = runLinter();
+
+  if (lintResults.fatal) {
+    console.log('>> Fatal error detected by lint <<'.red);
+    stopServer();
+    console.log('>> FIX FATAL ERRORS TO START SERVER <<'.red);
+    return;
+  }
+
+  if (lintResults.fixes) ignoreNextChange = true;
+  startServer();
+}
+
+// watch files for changes, run linter and restart server
+chokidar.watch(nodemonConfig.watch, {
+  ignored: [
+    'node_modules', // ignore node_modules
+    /(^|[/\\])\../, // ignore anything starting with .
+    ...nodemonConfig.ignore || [],
+  ],
+}).on('change', (event, path) => {
+  lintAndRun();
+});
+
+lintAndRun();
+
+//
+// pass through exit to running app process
+function exit() {
+  console.log('>>> Shutting down dev server <<<'.red);
+  nodemon.emit('quit');
+  process.exit();
+}
+// explicitly exit on signal
+process.on('SIGTERM', exit);
+process.on('SIGINT', exit);
